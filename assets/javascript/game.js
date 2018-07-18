534adf5febbfcd9fe478a817eefa68ff0b6ca0e3
mattshin <matthewshin@utexas.edu>
2018-07-18 00:06:26 -0500

    Fix: random gems on reset, score display on init, style guidelines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,41 @@
-var random = Math.floor(Math.random() * 102) + 19;
+var target = Math.floor(Math.random() * 102) + 19;
 var userTotal = 0; 
 var wins = 0; 
 var losses = 0; 
 var gems = $("#gems").children();
-var gemValues = [];
+var gemValues = {};
+var colors = ["blue", "green", "red", "yellow"];
+$("#randomNumber").text(target);
 
 
-var blue= Math.floor(Math.random()*12+1)
-var red= Math.floor(Math.random()*12+1)
-var green= Math.floor(Math.random()*12+1)
-var yellow= Math.floor(Math.random()*12+1)
+function setGemValues(){
+    for(var color of colors){
+        gemValues[color] = Math.floor(Math.random() * 12 + 1);
+    }
+}
 
-$("#randomNumber").text(random);
-
-$.map(gems, function(){
-    gemValues.push(Math.floor(Math.random()*12)+1);
-});
-$("wins").text(wins); 
-$("losses").text(losses);
+setGemValues();
+$("#wins").text(wins); 
+$("#losses").text(losses);
 // This will hold all of the user's score and total points. Now grab it in text with jQuery
 // Added the random number var so user can guess // 
-function reset(){
-    random = Math.floor(Math.random()*102)+19;
+function reset() {
+    target = Math.floor(Math.random() * 102) + 19;
     userTotal = 0; 
-    console.log(random);
-    $("#randomNumber").text(random);
+    
+    $("#randomNumber").text(target);
     $("#finalScore").text(userTotal);
-    $.map(gems, function(){
-        gemValues.push(Math.floor(Math.random()*12)+1);
-    });
+    setGemValues();
 }
 // Above I added reset function to start,reset game, and make the random numbers guess while including jQuery to 
 // display random number and final score onto display 
-function winner(){
+function winner() {
     wins++;
     $("#wins").text(wins); 
     reset();
 }
-function Loser() {
+
+function loser() {
     losses++;
     $("#losses").text(losses);
     reset();
@@ -47,73 +45,59 @@ function Loser() {
 // Jquery will follow after to add a tally and reset the whole game.
 // Below add click buttons for crystals, make sure to add up each time you click
 function calculateScore() {
-    if (userTotal === random){
+    if (userTotal === random) {
         winner();
         $("#wins").text(wins);
         console.log(wins);
-    } 
-    else if (userTotal > random){
+    } else if (userTotal > random) {
         losses();
         $('#losses').text(losses);
-		        console.log(losses)
+        console.log(losses)
     }
 
 
 }
 // Below create win losses with if else to see if their score matches up or not with randomNumber// 
-$('#blue').on ('click', function(){
-    userTotal = userTotal + blue;
+$('#blue').on ('click', function() {
+    userTotal = userTotal + gemValues.blue;
     console.log("New userTotal= " + userTotal);
     $("#finalScore").text(userTotal); 
-        if (userTotal === random){
-          winner();
-        }
-        else if ( userTotal > random){
-          Loser();
-        }   
-
-        $("#randomNumber").text(random);
+    if (userTotal === target){
+       winner();
+    } else if ( userTotal > target) {
+       loser();
+    }
   });
 
-  $("#green").on ('click', function(){
-    userTotal = userTotal + green;
+ $("#green").on ('click', function() {
+    userTotal = userTotal + gemValues.green;
     console.log("New userTotal= " + userTotal);
     $("#finalScore").text(userTotal); 
-        if (userTotal === random){
-          winner();
-        }
-        else if ( userTotal > random){
-            Loser();
-        } 
-
-    $("#randomNumber").text(random);
+    if (userTotal === target){
+       winner();
+    } else if ( userTotal > target) {
+       loser();
+    }
   });
 
-  $("#red").on ('click', function(){
-    userTotal = userTotal + red;
+ $("#red").on ('click', function() {
+    userTotal = userTotal + gemValues.red;
     console.log("New userTotal= " + userTotal);
     $("#finalScore").text(userTotal);
-          if (userTotal === random){
-          winner();
-        }
-        else if ( userTotal > random){
-            Loser();
-        } 
-
-    $("#randomNumber").text(random);
+    if (userTotal === target){
+       winner();
+    } else if ( userTotal > target) {
+       loser();
+    }
   });   
 
-  $("#yellow").on ('click', function(){
-    userTotal = userTotal + yellow;
+ $("#yellow").on ('click', function() {
+    userTotal = userTotal + gemValues.yellow;
     console.log("New userTotal= " + userTotal);
     $('#finalScore').text(userTotal); 
-      
-          if (userTotal === random){
-          winner();
-        }
-        else if ( userTotal > random){
-            Loser();
-        }
-
-    $("#randomNumber").text(random);
+    if (userTotal === target){
+       winner();
+    } else if ( userTotal > target) {
+       loser();
+    }
   });   
